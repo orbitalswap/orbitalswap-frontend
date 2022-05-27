957e46db725ca9bd2a64f1b85c14b3db884dcab4
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-05-28 03:06:17 +0800

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -4,8 +4,8 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'OrbitalSwap',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by OrbitalSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+    'The Future of Defi, The Next Generation In Defi Tools and Trading',
+  image: 'https://orbitalswap.netlify.app/images/hero.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
