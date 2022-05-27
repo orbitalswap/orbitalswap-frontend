7c8159aeaecd0113c4b772f83572b110cb36762e
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-05-28 03:07:50 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,16 +54,16 @@ function MyApp(props: AppProps) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover OrbitalSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="Orbital - The Future of DeFi"
         />
         <meta name="theme-color" content="#38D7CD" />
-        <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
+        <meta name="twitter:image" content="https://orbitalswap.netlify.app/images/hero.png" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by OrbitalSwap), NFTs, and more, on a platform you can trust."
+          content="Orbital - The Future of DeFi, The Next Generation In Defi Tools and Trading."
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 OrbitalSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
+        <meta name="twitter:title" content="OrbitalSwap - Orbital - The Future of DeFi" />
         <title>OrbitalSwap</title>
       </Head>
       <Providers store={store}>
