6338dcd7baa6af7c46baa7938df218b9a3a07bbd
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-06-04 19:47:17 +0800

    remove cake for common bases

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -46,7 +46,7 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
-  [ChainId.MAINNET]: [mainnetTokens.busd, mainnetTokens.cake, mainnetTokens.btcb],
+  [ChainId.MAINNET]: [mainnetTokens.busd, mainnetTokens.btcb],
   [ChainId.TESTNET]: [testnetTokens.wbnb, testnetTokens.cake, testnetTokens.busd],
 }
 
