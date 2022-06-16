e2f7313d7820f1e3d665b4dc932e81950a9db0a7
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-06-16 11:25:32 +0800

    disable dibs launchpad

--- src/config/constants/launchpads.ts
+++ src/config/constants/launchpads.ts
@@ -19,23 +19,23 @@ const launchpads: SerializedLaunchpadConfig[] = [
     releaseAt: 1652986800,
     token: serializedTokens.tytan,
   },
-  {
-    id: 2,
-    address: {
-      97: '0x6B2233381D667F781bc39d95600BCC440816209a',
-      56: '0xBa809c35E9314E89a82de6d7Ccf9DC3f4C0CA28E',
-    },
-    isActive: true,
-    isTomFork: true,
-    name: 'USdibs',
-    subTitle: 'Auto-Staking & Compounding From the Future',
-    description:
-      "USDibs is an algorithmic stablecoin pegged to the price of BUSD",
-    projectSiteUrl: 'https://dibs.money/',
-    releaseAt: 1652986800,
-    currency: serializedTokens.busd,
-    token: serializedTokens.usdibs,
-  },
+  // {
+  //   id: 2,
+  //   address: {
+  //     97: '0x6B2233381D667F781bc39d95600BCC440816209a',
+  //     56: '0xBa809c35E9314E89a82de6d7Ccf9DC3f4C0CA28E',
+  //   },
+  //   isActive: true,
+  //   isTomFork: true,
+  //   name: 'USdibs',
+  //   subTitle: 'Auto-Staking & Compounding From the Future',
+  //   description:
+  //     "USDibs is an algorithmic stablecoin pegged to the price of BUSD",
+  //   projectSiteUrl: 'https://dibs.money/',
+  //   releaseAt: 1652986800,
+  //   currency: serializedTokens.busd,
+  //   token: serializedTokens.usdibs,
+  // },
 ]
 
 export default launchpads
