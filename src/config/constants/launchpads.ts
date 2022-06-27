f071d7bd93a5b8ced222960a1dfd5df64564f341
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-06-27 23:08:30 +0800

    relaunched SMG private sale

--- src/config/constants/launchpads.ts
+++ src/config/constants/launchpads.ts
@@ -40,6 +40,24 @@ const launchpads: SerializedLaunchpadConfig[] = [
     currency: serializedTokens.usdc,
     token: serializedTokens.smg,
   },
+  {
+    id: 3,
+    address: {
+      338: '0x3e70a810a40119abeF47F885CD6739f9C0eF4D3D',
+      25: '0xE4C7Ef82824Dc78c3f1Fe542E2eC171C6cf9bC2f',
+    },
+    isActive: true,
+    isTomFork: true,
+    isPrivatesale: true,
+    name: 'Smoking Gorilla',
+    subTitle: 'Auto-Staking & Compounding From the Future',
+    description:
+      "Smoking Gorilla Token is an Auto Rebasing Token with a Auto Decaying APY model which reduces the APY automatically within 6 months and then slowly keeps decaying to find a realistic high APY that is sustainable.",
+    projectSiteUrl: 'https://smokingorilla.finance/',
+    releaseAt: 1652986800,
+    currency: serializedTokens.usdc,
+    token: serializedTokens.smg,
+  },
 ]
 
 export default launchpads
