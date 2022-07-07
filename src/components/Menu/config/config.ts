7889aa5c42a3fa672bcee0e1085247acc7150c23
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-07-07 18:31:28 +0800

    Update config.ts

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -65,7 +65,7 @@ const config: (t: ContextApi['t'], languageCode?: string) => ConfigMenuItemsType
   },
   {
     label: t('Win'),
-    href: '/prediction',
+    href: '/lottery',
     icon: TrophyIcon,
     fillIcon: TrophyFillIcon,
     items: [
@@ -74,10 +74,10 @@ const config: (t: ContextApi['t'], languageCode?: string) => ConfigMenuItemsType
       //   href: '/competition',
       //   hideSubNav: true,
       // },
-      {
-        label: t('Prediction'),
-        href: '/prediction',
-      },
+      // {
+      //   label: t('Prediction'),
+      //   href: '/prediction',
+      // },
       {
         label: t('Lottery'),
         href: '/lottery',
