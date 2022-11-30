df64688d8d05c9fa4c79adc7a6f7db593dcaa6ab
miguelangelrm <miguelangelrm31@gmail.com>
2022-11-30 21:48:42 +0100

    janky redirect 🤷🏻‍♂️

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import Swap from '../views/Swap'
 
 const SwapPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/')
+  }, [])
   return <Swap />
 }
 
